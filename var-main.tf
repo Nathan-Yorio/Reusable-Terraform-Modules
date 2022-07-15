f3dd42170756687936ec539b9e2792925ca48ed9
Nathan-Yorio <>
2022-07-15 15:36:51 -0400

    removed some comments unrelated about elasticsearch, re-used the script

--- var-main.tf
+++ var-main.tf
@@ -1,10 +1,8 @@
 variable "region_id" {}
 
-#Elasticsearch provisioning variables#############################
 variable "route53_subdomain_name" {}
 variable "route53_domain_name" {}
 
-# Elastic load balancer name can probably be imported automatically
 # but for now I'm setting it manually
 # should concatenate the name of the LB to ".elb.amazonaws.com."
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elb#zone_id
